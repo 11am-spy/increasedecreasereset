09cd712a38ff08ee6370bd5ddbc2c577d974bbce
maninlov <128026663+11am-spy@users.noreply.github.com>
2026-06-07 22:18:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
+    const decreaseBtn = document.getElementById("decreaseBtn");
+    const resetBtn = document.getElementById("resetBtn");
+    const increaseBtn = document.getElementById("increaseBtn");
+    const countLabel = document.getElementById("countLabel");
+    const darkModeBtn = document.getElementById("darkModeBtn");
+    let count = 0;
 
-increaseBtn.onclick = function(){
-    count++;
-    countLabel.textContent = count; 
-}
+    increaseBtn.onclick = function(){
+        count++;
+        countLabel.textContent = count; 
+    }
 
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count; 
-}
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count; 
-}
+    decreaseBtn.onclick = function(){
+        count--;
+        countLabel.textContent = count; 
+    }
+    resetBtn.onclick = function(){
+        count = 0;
+        countLabel.textContent = count; 
+    }
+    darkModeBtn.onclick = function(){
+        document.body.classList.toggle("dark-theme");
+        if(document.body.classList.contains("dark-theme")){
+            darkModeBtn.textContent = "☀️ Light Mode";
+        } else {
+        darkModeBtn.textContent = "🌙 Dark Mode";
+    }
+    }
